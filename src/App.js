0cc967657d14abd74d7d8202ad6a9f76083f0bb1
igonzaleztb <ignacio.gonzalez@thebridgeschool.es>
2022-02-04 14:18:52 +0100

    Hola mundo React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>      Hola mundo</h1>
+<h2>      Hola mundo</h2>
+<h3>      Hola mundo</h3>
+<h4>      Hola mundo</h4>
+    </div>
+  );
+}
+
+export default App;
